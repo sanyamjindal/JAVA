d1cf130d798d3e345312e95ba51479c59703287b
sanyamjindal <101120349+sanyamjindal@users.noreply.github.com>
2022-04-20 23:53:17 +0530

    calc

---
+++ calculator.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class calculator {
+    public static void main(String[] args){
+        double add,sub,pro,div;
+        double a,b;
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter num1");
+        a = obj.nextInt();
+
+        System.out.println("Enter num2");
+        b = obj.nextInt();
+
+        System.out.println("Eneter your choice \n1.Addition \n2.Subtraction \n3.Multiplication \n4.Division\n");
+      
+        int choice = obj.nextInt();
+        System.out.println("You Entered: " + choice);
+        if (choice == 1)
+        {
+            add = a+b;
+        System.out.println("\nAddition is: " + add);
+        }
+        else if (choice == 2)
+        {
+            sub = a-b;
+        System.out.println("\nDifference is: " + sub);
+        }
+        else if (choice == 3)
+        {
+            pro = a*b;
+        System.out.println("\nProduct is: " + pro);
+        }
+        else if (choice == 4)
+        {
+            div = a/b;
+        System.out.println("\nQuotient is: " + div);
+        }
+
+        else
+        System.out.println("Invalid input");
+    }
+}
